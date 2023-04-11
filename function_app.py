5be44d11959d90a1a5661ba0bf7ba9c623afc300
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2023-04-11 17:13:52 +0530

    new: initial commit

---
+++ function_app.py
@@ -0,0 +1,31 @@
+import azure.functions as func
+from azure.data.tables import TableServiceClient, UpdateMode
+import os
+import logging
+
+app = func.FunctionApp()
+CONN_STR = os.environ["CounterDBConnStr"]
+
+# Learn more at aka.ms/pythonprogrammingmodel
+
+# Get started by running the following code to create a function using a HTTP trigger.
+
+
+@app.function_name("hit")
+@app.route(route="api", auth_level=func.AuthLevel.ANONYMOUS)
+def hit(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    table_service_client = TableServiceClient.from_connection_string(
+        conn_str=CONN_STR)
+    table_name = "Counter"
+    table_client = table_service_client.get_table_client(table_name=table_name)
+    curr_count = table_client.get_entity(partition_key="1", row_key="1")
+    curr_count = curr_count["Count"]
+    curr_count += 1
+    table_client.update_entity(mode=UpdateMode.REPLACE, entity={
+                               "PartitionKey": "1", "RowKey": "1", "Count": curr_count})
+    return func.HttpResponse(
+        str(curr_count),
+        status_code=200
+    )
